2cc9660b389cfe4f57d2835a6268ca83a5155977
Pasco <Pasco@163.com>
2020-11-08 11:51:15 +0800

    login - new layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 Vue.use(VueRouter)
+import login from '@/view/login/login.vue'
 const router = new VueRouter({
     routes: [
         // 这里编写相应路由规则
         // 登录模块规则
         {
             path:"/login",
+            component:login
+        },
+        {
+            path:"/",
             component:login
         }
     ]
