74e3a91d5a767989630da46ab624eec1dd53dd49
Pasco <Pasco@163.com>
2020-11-08 10:58:59 +0800

    project - new EliementUI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//导入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//使用插件
+Vue.use(ElementUI);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
